08acfc7c2e6aaa4580b0a75808b73a07d4d54c32
Thierry Fleury <thierry@codinlab.fr>
2016-10-09 15:18:21 +0200

    Add ServiceProvider

---
+++ src/Yeast.DependencyInjection.Autofac/ServiceProvider.cs
@@ -0,0 +1,42 @@
+﻿using Autofac;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Yeast.DependencyInjection.Autofac
+{
+    public class ServiceProvider : IServiceProvider, ISupportRequiredService
+    {
+        private readonly IComponentContext _componentContext;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServiceProvider"/> class.
+        /// </summary>
+        /// <param name="componentContext">The <see cref="IComponentContext"/> from which services should be resolved.</param>
+        public ServiceProvider(IComponentContext componentContext)
+        {
+            _componentContext = componentContext;
+        }
+
+        /// <summary>
+        /// Gets service of type <paramref name="serviceType"/> from the <see cref="IServiceProvider"/> implementing
+        /// this interface.
+        /// </summary>
+        /// <param name="serviceType">An object that specifies the type of service object to get.</param>
+        /// <returns>A service object of type <paramref name="serviceType"/>.
+        /// Throws an exception if the <see cref="IServiceProvider"/> cannot create the object.</returns>
+        public object GetRequiredService(Type serviceType)
+        {
+            return _componentContext.Resolve(serviceType);
+        }
+
+        /// <summary>
+        /// Gets the service object of the specified type.
+        /// </summary>
+        /// <param name="serviceType">Type of the requested service</param>
+        /// <returns>A service object of type <paramref name="serviceType" /> or null</returns>
+        public object GetService(Type serviceType)
+        {
+            return _componentContext.ResolveOptional(serviceType);
+        }
+    }
+}
---
+++ src/Yeast.DependencyInjection.StructureMap/ServiceProvider.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StructureMap;
+using System;
+
+namespace Yeast.DependencyInjection.StructureMap
+{
+    public class ServiceProvider : IServiceProvider, ISupportRequiredService
+    {
+        private readonly IContainer _container;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServiceProvider"/> class.
+        /// </summary>
+        /// <param name="IContainer">The <see cref="IContainer"/> from which services should be resolved.</param>
+        public ServiceProvider(IContainer container)
+        {
+            _container = container;
+        }
+
+        /// <summary>
+        /// Gets service of type <paramref name="serviceType"/> from the <see cref="IServiceProvider"/> implementing
+        /// this interface.
+        /// </summary>
+        /// <param name="serviceType">An object that specifies the type of service object to get.</param>
+        /// <returns>A service object of type <paramref name="serviceType"/>.
+        /// Throws an exception if the <see cref="IServiceProvider"/> cannot create the object.</returns>
+        public object GetRequiredService(Type serviceType)
+        {
+            // From StructureMap.Microsoft.DependencyInjection
+            /*if (serviceType.IsGenericEnumerable())
+            {
+                return GetRequiredService(serviceType);
+            }*/
+            return _container.TryGetInstance(serviceType);
+        }
+
+        /// <summary>
+        /// Gets the service object of the specified type.
+        /// </summary>
+        /// <param name="serviceType">Type of the requested service</param>
+        /// <returns>A service object of type <paramref name="serviceType" /> or null</returns>
+        public object GetService(Type serviceType)
+        {
+            return _container.GetInstance(serviceType);
+        }
+    }
+}
