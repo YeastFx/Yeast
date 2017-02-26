03f49d95a0b650431d383b570c3cc39c50cd7293
Thierry Fleury <thierry@codinlab.fr>
2017-02-26 20:54:01 +0100

    Remove samples
    Moved to Samples repository

--- samples/MultitenantApp/Multitenancy/SampleMultitenancyOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.ObjectModel;
-
-namespace MultitenantApp.Multitenancy
-{
-    public class SampleMultitenancyOptions
-    {
-        public Collection<SampleTenant> Tenants { get; set; }
-    }
-}
