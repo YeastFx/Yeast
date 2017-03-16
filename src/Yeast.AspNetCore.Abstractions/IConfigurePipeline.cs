55e9e688c473d7d88255528ad5b195428840eb7b
Thierry Fleury <thierry@codinlab.fr>
2017-03-16 11:59:11 +0100

    Add Yeast.AspNetCore.Abstractions project

---
+++ src/Yeast.AspNetCore.Abstractions/IConfigurePipeline.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Yeast.AspNetCore.Abstractions
+{
+    public interface IConfigurePipeline
+    {
+        void Configure(IApplicationBuilder app);
+    }
+}
