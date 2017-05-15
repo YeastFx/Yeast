d2ca7609da3aed69be485b35afe08f41f172e828
Thierry Fleury <thierry@codinlab.fr>
2017-05-15 10:23:10 +0200

    Add ApiRequestParams

---
+++ src/Yeast.WebApi/Data/ApiRequestParams.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Yeast.WebApi.Data
+{
+    /// <summary>
+    /// Base class for API request parameters
+    /// </summary>
+    public class ApiRequestParams
+    {
+        /// <summary>
+        /// List of navigation properties to include
+        /// </summary>
+        [FromQuery(Name = "$include")]
+        public string[] Include { get; set; }
+    }
+}
