5d8a7bd0b0c7313dc7ff4cd553a9ffd97db1e16b
Thierry Fleury <thierry@codinlab.fr>
2017-07-10 12:40:09 +0200

    Make generic parameter optional

--- src/Yeast.WebApi/QueryResult.cs
+++ src/Yeast.WebApi/QueryResult.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json;
+﻿using System.Collections;
 using System.Collections.Generic;
 
 namespace Yeast.WebApi
@@ -6,18 +6,23 @@ namespace Yeast.WebApi
     /// <summary>
     /// Class to provide query results
     /// </summary>
+    public class QueryResult : QueryResultBase
+    {
+        /// <summary>
+        /// Paged query results
+        /// </summary>
+        public IEnumerable Data { get; set; }
+    }
+
+    /// <summary>
+    /// Class to provide query results
+    /// </summary>
     /// <typeparam name="T">Queried object type</typeparam>
-    public class QueryResult<T>
+    public class QueryResult<T> : QueryResultBase
     {
         /// <summary>
         /// Paged query results
         /// </summary>
         public IEnumerable<T> Data { get; set; }
-
-        /// <summary>
-        /// Total number of results
-        /// </summary>
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-        public int? TotalResults { get; set; }
     }
 }
---
+++ src/Yeast.WebApi/QueryResultBase.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Yeast.WebApi
+{
+    /// <summary>
+    /// QueryResult base class
+    /// </summary>
+    public abstract class QueryResultBase
+    {
+        /// <summary>
+        /// Total number of results
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public int? TotalResults { get; set; }
+    }
+}
